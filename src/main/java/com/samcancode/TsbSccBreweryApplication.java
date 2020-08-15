e0609ba6697b6832eb790fe5ad0deeec6c1467ff
Sam Lau <samlau.sam@gmail.com>
2020-08-15 19:32:36 +0800

    initial project setup

---
+++ src/main/java/com/samcancode/TsbSccBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.samcancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsbSccBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsbSccBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samcancode/TsbSccBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samcancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsbSccBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
